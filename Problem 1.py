71f9557f310c6df42c9af4aaf2f0a7e19f49ef63
LeoAndriolo <leonardoandriolo.la@gmail.com>
2023-08-16 16:40:53 -0300

    solution to P01

---
+++ Problem 1.py
@@ -0,0 +1,15 @@
+def fibonacci(n):
+    a = 0
+    b = 1
+    for i in range(n):
+        a, b = b, a + b
+    return a
+
+def main():
+    n = int(input("Input: How Many Terms do you want to Display: "))
+    print("Output: ".format(n), end="")
+    for i in range(n):
+        print(fibonacci(i)," ", end="")
+
+if __name__ == "__main__":
+    main()
