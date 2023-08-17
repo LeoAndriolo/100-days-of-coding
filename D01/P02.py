7f62b9822c031ea7948a28272542d847141428df
LeoAndriolo <leonardoandriolo.la@gmail.com>
2023-08-17 11:00:35 -0300

    example added

--- D01/P02.py
+++ D01/P02.py
@@ -12,3 +12,9 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+# def factorial(n):
+#     return 1 if (n == 0 or n == 1) else n*factorial(n-1)
+
+# n = int(input("Enter a Number: "))
+# print(f"{n}! = {factorial(n)}")
